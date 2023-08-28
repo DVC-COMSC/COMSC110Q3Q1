29e4a40df888b318823a21797491aabb08ab1efc
Kyuwoong Lee <kwooler@gmail.com>
2023-08-27 18:42:25 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int num1, num2, num3;
+  
+  /* ************************************
+    Code your program here
+    Take three user input
+    Calculate sum and average
+    print the result with two precision number
+      Input Example 1
+      10.0 20.0 30.0
+      Output Example 1
+      60.00 20.00
+  ************************************ */
+
+  cout << setprecision(2) << fixed;
+
+}
